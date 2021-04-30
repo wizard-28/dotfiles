4b6c84fa1b53a4b99427868174b60c822459e675
Sourajyoti Basak <basak.sb2006@gmail.com>
2021-04-30 15:19:04 +0530

    Comply with shellcheck as much as possible

--- setup.sh
+++ setup.sh
@@ -178,10 +178,10 @@ checkpoint "Proceeding with git configuration..."
 process "Adding SSH and GPG keys..."
 sudo cp -r /media/pop-os/S\ BASAK/.ssh/ ~/.ssh/ 
 sudo cp /media/pop-os/S BASAK/github.asc ~/github.asc
-sudo chown $USER:$USER ~/.ssh/id_ed25519*
+sudo chown "$USER":"$USER" ~/.ssh/id_ed25519*
 chmod 600 ~/.ssh/id_ed25519
 chmod 644 ~/.ssh/id_ed25519.pub
-exec ssh-agent bash
+ssh-agent bash
 ssh-add ~/.ssh/id_ed25519
 gpg --import github.asc
 success "SSH and GPG keys added" "adding SSH and GPG Keys"
