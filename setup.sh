66959dcd394454ce2ac007ccff552f527f3f8e90
Sourajyoti Basak <basak.sb2006@gmail.com>
2021-04-30 14:13:37 +0530

    Minor changes

--- setup.sh
+++ setup.sh
@@ -123,7 +123,6 @@ sudo apt-get install waybar -y > /dev/null
 success "Waybar installed" "installing waybar"
 # Install fonts
 process "Installing fonts for waybar..."
-# TODO: Install losevka
 sudo apt-get install fonts-font-awesome -y > /dev/null
 fc-cache -f -v > /dev/null
 success "Fonts installed for waybar" "installing fonts for waybar"
