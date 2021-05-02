dab147a19b550532b5bfb59191897824e35d248b
Sourajyoti Basak <basak.sb2006@gmail.com>
2021-05-02 13:21:04 +0530

    Configure SSH

--- setup.sh
+++ setup.sh
@@ -171,6 +171,11 @@ git config --global user.name "Sourajyoti Basak"
 git config --global user.email "basak.sb2006@gmail.com"
 git config --global user.signingkey CFF8C32DEBE58AB4
 git config --global commit.gpgsign true
+# Setup SSH
+sudo touch .ssh/known_hosts
+cd dotfiles
+git remote set-url origin git@github.com:wizard-28/dotfiles.git
+cd ..
 success "Git configured" "configuring git"
 
 
