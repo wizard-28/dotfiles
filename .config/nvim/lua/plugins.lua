517143ac8d944643807a9f61f981d3834a7d076f
Sourajyoti Basak <wiz28@protonmail.com>
2021-07-15 19:47:17 +0530

    feat(vim): Lazy-load plugins and cleanup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,39 +15,106 @@
 cmd 'packadd packer.nvim'
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'		-- Packer itself
+	use { 'wbthomason/packer.nvim', opt = true }		-- Packer itself
 
 	-- General
 	use 'sainnhe/gruvbox-material'
 	use {
 		'nvim-telescope/telescope.nvim',
-		config = [[require('plugin.telescope')]],
+		config = function()
+			require('plugin.telescope')
+		end,
+		cmd = { 'Telescope' },
+		keys = { '<leader>f' },
 		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'},
 			{'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }}
 	}
-	use { "folke/which-key.nvim", config = function() require("which-key").setup {} end }
-	use 'ggandor/lightspeed.nvim'
-	use { 'lambdalisue/suda.vim', setup = function() g.suda_smart_edit = true end }
-	use { 'lukas-reineke/indent-blankline.nvim', event = 'BufReadPre', setup = [[require('plugin.blankline')]] }
+	use {
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup {}
+		end
+	}
+	use {
+		'ggandor/lightspeed.nvim',
+		keys = { 's', 'S' }
+	}
+	use {
+		'lambdalisue/suda.vim',
+		setup = function()
+			g.suda_smart_edit = true
+		end
+	}
+	use {
+		'lukas-reineke/indent-blankline.nvim',
+		event = 'BufReadPre',
+		setup = function()
+			require('plugin.blankline')
+		end
+	}
+	use {
+		'beauwilliams/statusline.lua',
+		requires = {
+			'kosayoda/nvim-lightbulb',
+			'kyazdani42/nvim-web-devicons'
+		}
+	}
 
 	-- Git
 	use {
 		'lewis6991/gitsigns.nvim',
-		requires = { 'nvim-lua/plenary.nvim' },
-		config = [[require('plugin.gitsigns')]]
+		event = 'BufReadPre',
+		requires = 'nvim-lua/plenary.nvim',
+		config = function()
+			require('plugin.gitsigns')
+		end
 	}
-	use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim', config = function() require('neogit').setup {} end }
+	use {
+		'TimUntersberger/neogit',
+		cmd = 'Neogit',
+		requires = 'nvim-lua/plenary.nvim',
+		config = function()
+			require('neogit').setup {}
+		end
+	}
 
 	-- LSP
-	use { 'neovim/nvim-lspconfig', config = [[require('plugin.lsp')]] }
-	use 'kabouzeid/nvim-lspinstall'
-	use { 'glepnir/lspsaga.nvim', config = [[require('plugin.lspsaga')]] }
-	use { 'hrsh7th/nvim-compe', config = [[require('plugin.compe')]], event = 'InsertEnter *' }
+	use {
+		'neovim/nvim-lspconfig',
+		event = 'BufReadPre',
+		requires = {{
+			'kabouzeid/nvim-lspinstall'
+		}, {
+				'glepnir/lspsaga.nvim',
+				config = function()
+					require('plugin.lspsaga')
+				end
+			}},
+		config = function()
+			require('plugin.lsp')
+		end
+	}
+	use {
+		'hrsh7th/nvim-compe',
+		event = 'InsertEnter',
+		config = function()
+			require('plugin.compe')
+		end
+	}
 	use 'L3MON4D3/LuaSnip'
-	use { 'mfussenegger/nvim-lint', ft = { 'sh' }, config = [[require('plugin.lint')]] }
-	use { 'nvim-treesitter/nvim-treesitter', ft = { 'sh', 'lua' }, config = [[require('plugin.treesitter')]], run = ':TSUpdate' }
-
-	-- Style
-	use { 'beauwilliams/statusline.lua', requires = { 'kosayoda/nvim-lightbulb' }}
-	use 'kyazdani42/nvim-web-devicons'	-- Icons
+	use {
+		'mfussenegger/nvim-lint',
+		ft = 'sh',
+		config = function()
+			require('plugin.lint')
+		end
+	}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		ft = { 'sh', 'lua' },
+		run = ':TSUpdate',
+		config = function()
+			require('plugin.treesitter')
+		end
+	}
 end)
