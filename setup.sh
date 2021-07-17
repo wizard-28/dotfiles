a9bf0ad53693878d28944091870f6e04205a9c4f
Sourajyoti Basak <wiz28@protonmail.com>
2021-07-17 15:53:49 +0530

    feat(setup): set `fish` as the default shell

--- setup.sh
+++ setup.sh
@@ -182,6 +182,7 @@ process "Installing fish..."
 sudo apt-add-repository ppa:fish-shell/release-3 -y > /dev/null
 sudo apt-get update -y > /dev/null
 sudo apt-get install fish -y > /dev/null
+chsh -s /usr/bin/fish
 success "Installed fish" "installing fish"
 
 # Install starship
