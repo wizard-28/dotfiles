3d2aacb4ab6c9d4d85c126d14391ae20703eada7
Sourajyoti Basak <wiz28@protonmail.com>
2021-08-27 17:37:34 +0530

    Add stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,14 @@
 
 fish_vi_cursor
 
+# git sync
+function gsync
+	git reset --hard HEAD~1
+	git push -f
+	git pull upstream $argv --rebase
+	git push
+end
+
 # Aliases
 alias l="exa --icons -F"
 alias la="exa --icons -aF"
