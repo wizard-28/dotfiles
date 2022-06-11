357db5bdef4fa6d5ae5bacea31daf5568b4e7a3c
Sourajyoti Basak <wiz28@protonmail.com>
2022-06-11 21:07:10 +0530

    refactor(faststrap)!: adapt to Fedora 36 WS

--- faststrap.sh
+++ faststrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # =============================================================================
 #  __      __________   ______
 # /  \    /  \_____  \ /  __  \
@@ -13,15 +13,17 @@
 # License: MIT
 # =============================================================================
 
+PERSISTENT_STORAGE_LOCATION="/run/media/liveuser/SBASAK"
+
 # =============================================================================
 # Functions
 # =============================================================================
 stage() {
-    echo "\n$(tput bold)[\e[32mSTAGE\e[0m$(tput bold)]$(tput sgr0) $1"
+    echo -e "\n$(tput bold)[\e[32mSTAGE\e[0m$(tput bold)]$(tput sgr0) $1"
 }
 
 info() {
-    echo "$(tput bold)[\e[34mINFO\e[0m$(tput bold)]$(tput sgr0) $1"
+    echo -e "$(tput bold)[\e[34mINFO\e[0m$(tput bold)]$(tput sgr0) $1"
 }
 # =============================================================================
 
@@ -29,11 +31,11 @@ info() {
 # Banner
 # =============================================================================
 tput bold
-printf '    _________   __________________________  ___    ____ 
+printf '    _________   __________________________  ___    ____
    / ____/   | / ___/_  __/ ___/_  __/ __ \/   |  / __ \
   / /_  / /| | \__ \ / /  \__ \ / / / /_/ / /| | / /_/ /
- / __/ / ___ |___/ // /  ___/ // / / _, _/ ___ |/ ____/ 
-/_/   /_/  |_/____//_/  /____//_/ /_/ |_/_/  |_/_/      
+ / __/ / ___ |___/ // /  ___/ // / / _, _/ ___ |/ ____/
+/_/   /_/  |_/____//_/  /____//_/ /_/ |_/_/  |_/_/
 '
 tput sgr0
 # =============================================================================
@@ -49,30 +51,17 @@ sudo timedatectl set-timezone Asia/Kolkata &
 info "Setting up SysRq"
 sudo sysctl -w kernel.sysrq=1 > /dev/null &
 
-info "Optimizing APT sources"
-echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt focal-updates main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt focal-backports main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt focal-security main restricted universe multiverse
-deb cdrom:[Pop_OS 20.04 _Focal Fossa_ - Release amd64 (20200702)]/ focal main restricted
-deb http://apt.pop-os.org/proprietary focal main" | sudo tee /etc/apt/sources.list > /dev/null
+info "Optimizing DNF"
+echo "fastestmirror=1
+max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 
-info "Adding PPAs"
-echo 'deb http://download.opensuse.org/repositories/home:/Head_on_a_Stick:/azote/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:Head_on_a_Stick:azote.list > /dev/null
-curl -fsSL https://download.opensuse.org/repositories/home:Head_on_a_Stick:azote/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_Head_on_a_Stick_azote.gpg > /dev/null || ../setup.sh
-sudo add-apt-repository ppa:git-core/ppa -yn > /dev/null || ../setup.sh
-sudo add-apt-repository ppa:nschloe/sway-backports -yn > /dev/null || ../setup.sh
-sudo add-apt-repository ppa:nschloe/waybar -yn > /dev/null || ../setup.sh
-sudo add-apt-repository ppa:dtl131/mpdbackport -yn > /dev/null || ../setup.sh
-sudo apt-add-repository ppa:fish-shell/release-3 -yn > /dev/null || ../setup.sh
-sudo add-apt-repository ppa:neovim-ppa/unstable -yn > /dev/null || ../setup.sh
-sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream -y > /dev/null || ../setup.sh
-
-info "Updating APT"
-sudo apt-get install apt -y > /dev/null || .../setup.sh
+info "Adding extra repos"
+sudo rpm --import https://keys.openpgp.org/vks/v1/by-fingerprint/034F7776EF5E0C613D2F7934D29FBD5F93C0CFC3
+sudo dnf config-manager --add-repo https://rpm.librewolf.net > /dev/null
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y > /dev/null
 
 info "Installing configuration files"
-sudo apt-get install xutils-dev -y > /dev/null || .../setup.sh
+sudo dnf install --refresh lndir -y > /dev/null
 (
     mkdir -p ~/.doom.d/ ~/.weechat/ ~/.librewolf/
     lndir -silent ~/dotfiles/.config/ ~/.config/
@@ -86,88 +75,71 @@ sudo apt-get install xutils-dev -y > /dev/null || .../setup.sh
 ) &
 
 info "Configuring environment variables"
-echo 'EDITOR="nvim"\nMOZ_ENABLE_WAYLAND=1' | sudo tee -a /etc/environment > /dev/null &
+echo 'EDITOR="nvim"
+MOZ_ENABLE_WAYLAND=1' | sudo tee -a /etc/environment > /dev/null &
 # =============================================================================
 
 # =============================================================================
 # Stage 2: Install applications
 # =============================================================================
-stage "Installing applications..."
 
-info "Installing Pacstall applications."
-curl -fsSL https://git.io/Jue3Z > pacstall-install.sh # Pacstall (develop branch installer)
-chmod +x ./pacstall-install.sh
-yes | sudo ./pacstall-install.sh > /dev/null 2>&1
-rm ./pacstall-install.sh
-pacstall -U pacstall develop > /dev/null
-pacstall -PI librewolf-app bemenu-git shfmt-bin shellharden-bin dunst treefetch-bin git-delta-deb > /dev/null 2>&1
-
-info "Installing APT applications"
-sudo apt-get install -o Dpkg::Options::="--force-overwrite" -y \
-    lua5.3 bat ripgrep fd-find fzf zram-config zram-tools gnome-tweaks gstreamer1.0-plugins-bad \
-    libnotify-bin jq light grim slurp playerctl htop wl-clipboard xwayland libgdk-pixbuf2.0-common libgdk-pixbuf2.0-bin gir1.2-gdkpixbuf-2.0 python3-pip \
-    sway swayidle sway-backgrounds azote \
-    waybar fonts-font-awesome \
-    shellcheck \
+info "Installing DNF applications"
+sudo dnf install --setopt=install_weak_deps=False -y \
+    librewolf bemenu git-delta \
+    bat exa ripgrep fd-find fzf zoxide \
+    libnotify jq light playerctl htop \
+    libseat swayidle wlr-randr azote swaylock clipman grimshot dunst \
+    https://ftp.lysator.liu.se/pub/opensuse/tumbleweed/repo/oss/x86_64/libxcb-errors0-1.0-1.25.x86_64.rpm \
+    waybar fontawesome-fonts \
+    ShellCheck \
     alacritty \
     fish \
-    newsboat \
-    neovim python3.8-venv universal-ctags \
-    mpd mpc ncmpcpp \
-    libfdk-aac1 libldacbt-abr2 libldacbt-enc2 libopenaptx0 \
-    gstreamer1.0-pipewire libpipewire-0.3-0 libpipewire-0.3-dev libpipewire-0.3-modules libspa-0.2-bluetooth libspa-0.2-dev libspa-0.2-jack libspa-0.2-modules pipewire pipewire-audio-client-libraries pipewire-bin pipewire-locales pipewire-tests > /dev/null
+    neovim gcc-c++ npm \
+    python3-pip
+
+info "Installing GIT applications"
+(
+    git clone --depth 1 https://github.com/foundObjects/zram-swap.git > /dev/null
+    cd zram-swap && sudo ./install.sh > /dev/null
+    cd ../ && rm -rf zram-swap
+) &
 
 info "Installing PIP applications"
 sudo pip3 install \
-    autotiling \
     pynvim black \
     cmake > /dev/null &
 
-info "Installing applications from USB"
-sudo install -Dm6755 /media/pop-os/SBASAK/swaylock /usr/local/bin/ &
-sudo install -Dm755 /media/pop-os/SBASAK/clipman /bin/ &
-
-# ncmpcpp 0.9.2
-(
-    sudo cp /media/pop-os/SBASAK/ncmpcpp/lib*.so* /usr/lib/x86_64-linux-gnu/                   # Install libs
-    sudo apt-get install -y /media/pop-os/SBASAK/ncmpcpp/libicu67_67.1-7_amd64.deb > /dev/null # Install deb dependency
-    sudo install -D /media/pop-os/SBASAK/ncmpcpp/ncmpcpp /usr/bin                              # Install ncmpcpp binary
-) &
-
 info "Installing CURL applications"
+
 # Fonts
 (
-    sudo curl -sfLo "/usr/share/fonts/truetype/JetBrains Mono NL Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/complete/JetBrains%20Mono%20NL%20Regular%20Nerd%20Font%20Complete.ttf
-    sudo curl -sfLo "/usr/share/fonts/truetype/JetBrains Mono NL Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/Italic/complete/JetBrains%20Mono%20NL%20Italic%20Nerd%20Font%20Complete.ttf
+    sudo mkdir -p /usr/share/fonts/jetbrainsmononl-nerd-font
+    sudo curl -sfLo "/usr/share/fonts/jetbrainsmononl-nerd-font/JetBrains Mono NL Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/complete/JetBrains%20Mono%20NL%20Regular%20Nerd%20Font%20Complete.ttf
+    sudo curl -sfLo "/usr/share/fonts/jetbrainsmononl-nerd-font/JetBrains Mono NL Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/Italic/complete/JetBrains%20Mono%20NL%20Italic%20Nerd%20Font%20Complete.ttf
     fc-cache -f
 ) &
 
-# Application
 (
-    curl -sS https://webinstall.dev/zoxide | bash > /dev/null
-) &
-(
-    sudo curl -sfLo /usr/local/bin/grimshot https://raw.githubusercontent.com/swaywm/sway/master/contrib/grimshot
-    sudo chmod +x /usr/local/bin/grimshot
-) &
-(
-    curl -sSL https://install.python-poetry.org | python3 - > /dev/null
+    mkdir /tmp/faststrap/
+    cd /tmp/faststrap/ || exit
+
+    curl -s https://api.github.com/repos/vaxerski/Hyprland/releases/latest \
+    | jq ".assets[0].browser_download_url" \
+    | cut -d '"' -f 2 \
+    | wget -qi -
+    tar -xf -- *.tar.gz > /dev/null
+
+    sudo mv Hyprland /usr/bin/
+    sudo mv hyprctl /usr/bin
+    sudo mv lib* /usr/lib64
+    sudo mv example/hyprland.desktop /usr/share/wayland-sessions/
+
+    cd ../ && sudo rm -rf faststrap
 ) &
 
-(
-    mkdir exa/
-    cd exa/ || exit
-    curl -s https://api.github.com/repos/ogham/exa/releases/latest | grep "browser_download_url" | grep "exa-linux-x86_64-v" | cut -d '"' -f 4 | wget -qi -
-    unzip -q exa*
-    sudo mv bin/exa /usr/local/bin
-    sudo mv man/exa.1 /usr/share/man/man1/
-    sudo mv completions/exa.fish /usr/share/fish/vendor_completions.d/
-    sudo mv completions/exa.bash /etc/bash_completion.d/
-    cd .. && rm -r exa/
-) &
+curl -sSL https://install.python-poetry.org | python3 - > /dev/null &
 
 notify-send --urgency critical "Interaction required"
-curl -sL install-node.now.sh/lts | sudo bash
 curl -fsSL https://starship.rs/install.sh | sudo sh
 # =============================================================================
 
@@ -175,24 +147,13 @@ curl -fsSL https://starship.rs/install.sh | sudo sh
 # Stage 3: Purge bloat
 # =============================================================================
 stage "Purging bloat..."
-
-info "Purging APT bloat"
-(
-    sudo apt-get purge firefox libreoffice-common geary totem suckless-tools -y > /dev/null
-    sudo apt-get autoremove --purge -y > /dev/null
-) &
+sudo dnf remove firefox libreoffice* -y > /dev/null &
 # =============================================================================
 
 # =============================================================================
 # Stage 4: Postconfigurations
 # =============================================================================
 stage "Starting postconfigurations..."
-
-info "Adding bat symlink"
-(
-    mkdir -p ~/.local/bin
-    ln -s /usr/bin/batcat ~/.local/bin/bat
-) &
 
 info "Changing default shell"
 (
@@ -210,13 +171,6 @@ info "Setting up ZSWAP"
     sudo zramswap start > /dev/null 2>&1
 ) &
 
-info "Setting up Pipewire"
-(
-    systemctl --user --now disable pulseaudio.socket pulseaudio.service > /dev/null 2>&1
-    systemctl --user mask pulseaudio > /dev/null 2>&1
-    systemctl --user --now enable pipewire.socket pipewire-pulse.socket pipewire.service pipewire-pulse.service pipewire-media-session.service > /dev/null 2>&1
-) &
-
 info "Setting up git"
 (
     # Setup remote
@@ -225,17 +179,16 @@ info "Setting up git"
 ) &
 
 info "Setting up SSH and GPG keys"
-sudo cp -r /media/pop-os/SBASAK/.ssh/ ~
+sudo cp -r "${PERSISTENT_STORAGE_LOCATION}/.ssh/" ~
 sudo chown "$USER":"$USER" ~/.ssh/id_ed25519*
 chmod 600 ~/.ssh/id_ed25519
 chmod 644 ~/.ssh/id_ed25519.pub
 eval "$(ssh-agent)" > /dev/null
 
 notify-send "Enter password for SSH and GPG key"
-xclip -selection c < /media/pop-os/SBASAK/SSH
 ssh-add ~/.ssh/id_ed25519
-xclip -selection c < /media/pop-os/SBASAK/GPG
-gpg --import /media/pop-os/SBASAK/GPG.asc
+wl-copy < "${PERSISTENT_STORAGE_LOCATION}/GPG.txt"
+gpg --import "${PERSISTENT_STORAGE_LOCATION}/GPG.asc"
 
 wait
 # =============================================================================
