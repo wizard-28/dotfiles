74e991802e307ce6040c4164bd2f4a58bfc2d282
Sourajyoti Basak <wiz28@protonmail.com>
2022-12-27 19:21:48 +0530

    Add .config/nvim/lua/user/plugins/null-ls.lua

---
+++ private_dot_config/nvim/lua/user/plugins/null-ls.lua
@@ -0,0 +1,11 @@
+return function(config)
+  local null_ls = require "null-ls"
+  vim.pretty_print(config)
+  -- NOTE: This configures shfmt to follow the Pacstall style guidelines.
+  -- TODO: Look into configuring this automatically with https://github.com/folke/neoconf.nvim/issues/5
+  config.sources = {
+    null_ls.builtins.formatting.shfmt.with {
+      args = { "-kp", "-bn", "-ci", "-sr", "-s" },
+    },
+  }
+end
