5754f4b2e57610c247687d2f4c12f16be13c2152
Sourajyoti Basak <wiz28@protonmail.com>
2023-01-24 21:26:32 +0530

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,8 +1,20 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 
 return {
   color_scheme = "Catppuccin Mocha",
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   unicode_version = 15,
+  mouse_bindings = {
+    {
+      event = { Down = { streak = 3, button = "Left" } },
+      action = action.SelectTextAtMouseCursor("SemanticZone"),
+      mods = "NONE",
+    },
+  },
+  keys = {
+    { key = "UpArrow", mods = "SHIFT", action = action.ScrollToPrompt(-1) },
+    { key = "DownArrow", mods = "SHIFT", action = action.ScrollToPrompt(1) },
+  },
 }
