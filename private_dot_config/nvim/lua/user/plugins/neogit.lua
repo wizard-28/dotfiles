2bac910d18ad6ca93866264153b6081f1dd3c17e
Sourajyoti Basak <wiz28@protonmail.com>
2023-04-24 12:02:38 +0530

    Update .config/nvim/lua/user/plugins/diffview.lua Update .config/nvim/lua/user/plugins/neogit.lua

--- private_dot_config/nvim/lua/user/plugins/neogit.lua
+++ private_dot_config/nvim/lua/user/plugins/neogit.lua
@@ -1,6 +1,6 @@
 return {
   "TimUntersberger/neogit",
-  dependencies = "nvim-lua/plenary.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
   cmd = "Neogit",
   event = "User AstroGitFile",
   config = true,
