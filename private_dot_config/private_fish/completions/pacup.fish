9345a3d9423f4a43f72e5c39c8745a045cd7f690
Sourajyoti Basak <wiz28@protonmail.com>
2024-11-15 15:54:16 +0530

    Add .config/fish/README.org Add .config/fish/completions/__gitnow_completions.fish Add .config/fish/completions/fisher.fish Add .config/fish/completions/flamegraph.fish Add .config/fish/completions/pacup.fish Add .config/fish/completions/replay.fish Add .config/fish/conf.d/abbr_tips.fish Add .config/fish/conf.d/autopair.fish Add .config/fish/conf.d/done.fish Add .config/fish/conf.d/.gitnow Add .config/fish/conf.d/gitnow.fish Add .config/fish/conf.d/gitnow_config.fish Add .config/fish/conf.d/puffer_fish_key_bindings.fish Add .config/fish/conf.d/sponge.fish Add .config/fish/fish_plugins Add .config/fish/fish_variables Add .config/fish/functions/__abbr_tips_bind_newline.fish Add .config/fish/functions/__abbr_tips_bind_space.fish Add .config/fish/functions/__abbr_tips_clean.fish Add .config/fish/functions/__abbr_tips_init.fish Add .config/fish/functions/__gitnow_config_file.fish Add .config/fish/functions/__gitnow_functions.fish Add .config/fish/functions/__gitnow_manual.fish Add .config/fish/functions/_autopair_backspace.fish Add .config/fish/functions/_autopair_insert_left.fish Add .config/fish/functions/_autopair_insert_right.fish Add .config/fish/functions/_autopair_insert_same.fish Add .config/fish/functions/_autopair_tab.fish Add .config/fish/functions/_puffer_fish_expand_bang.fish Add .config/fish/functions/_puffer_fish_expand_dots.fish Add .config/fish/functions/_puffer_fish_expand_lastarg.fish Add .config/fish/functions/_sponge_clear_state.fish Add .config/fish/functions/_sponge_on_exit.fish Add .config/fish/functions/_sponge_on_postexec.fish Add .config/fish/functions/_sponge_on_preexec.fish Add .config/fish/functions/_sponge_on_prompt.fish Add .config/fish/functions/_sponge_remove_from_history.fish Add .config/fish/functions/fisher.fish Add .config/fish/functions/replay.fish Add .config/fish/functions/sponge_filter_failed.fish Add .config/fish/functions/sponge_filter_matched.fish Add .config/fish/themes/Catppuccin Frappe.theme Add .config/fish/themes/Catppuccin Latte.theme Add .config/fish/themes/Catppuccin Macchiato.theme Add .config/fish/themes/Catppuccin Mocha.theme

---
+++ private_dot_config/private_fish/completions/pacup.fish
@@ -0,0 +1,1 @@
+complete --command pacup --no-files --arguments "(env _PACUP_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) pacup)" --condition "env _PACUP_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) pacup"
