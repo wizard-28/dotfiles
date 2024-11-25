a73c7438491833a979e837db281ff14b79515fb8
Sourajyoti Basak <wiz28@protonmail.com>
2024-11-25 18:46:36 +0530

    Update .config/hypr/hyprland.conf Update .config/nvim/lazy-lock.json Update .config/waybar/config Update .config/waybar/config.toml Update .config/waybar/transpiler.py Update .config/waybar/style.css

--- private_dot_config/waybar/executable_transpiler.py
+++ private_dot_config/waybar/executable_transpiler.py
@@ -8,3 +8,7 @@ if __name__ == "__main__":
     with open(Path("~/.config/waybar/config.toml").expanduser(), "rb") as toml:
         with open(Path("~/.config/waybar/config").expanduser(), "w") as json:
             dump(load(toml), json, indent=4)
+
+    with open(Path("~/.config/waybar/modules.toml").expanduser(), "rb") as toml:
+        with open(Path("~/.config/waybar/modules.json").expanduser(), "w") as json:
+            dump(load(toml), json, indent=4)
