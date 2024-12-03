8f74b864a3f76bbda27ac3bfb9449d1d29ab42d9
Sourajyoti Basak <wiz28@protonmail.com>
2024-12-03 13:17:23 +0530

    Update .zshrc Update .config/hypr/hyprland.conf Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/options.lua Update .config/waybar/config Update .config/waybar/config.toml Update .config/waybar/style.css

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -5,6 +5,6 @@
 vim.g.maplocalleader = ","
 -- vim.g.lazyvim_python_lsp = "basedpyright"
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font"
+  vim.o.guifont = "JetBrainsMono Nerd Font:h11"
   vim.opt.linespace = 1
 end
